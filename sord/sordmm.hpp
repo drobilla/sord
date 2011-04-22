6819b93133c8baf871beab9ce7cdac039bace6f1
David Robillard <d@drobilla.net>
2011-04-22 08:25:40 +0000

    Switch to ISC license (legally equivalent to MIT license but with less obnoxious lawyer noise).
    git-svn-id: http://svn.drobilla.net/sord/trunk@77 3d64ff67-21c5-427c-a301-fe4f08042e5a

--- sord/sordmm.hpp
+++ sord/sordmm.hpp
@@ -1,23 +1,23 @@
-/* Sord, a lightweight RDF model library.
- * Copyright 2010-2011 David Robillard <d@drobilla.net>
- *
- * Sord is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Sord is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+/*
+  Copyright 2011 David Robillard <http://drobilla.net>
 
-/** @file sordmm.hpp
- * Public Sord C++ API.
- */
+  Permission to use, copy, modify, and/or distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THIS SOFTWARE IS PROVIDED  AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+/**
+   @file sordmm.hpp
+   Public Sord C++ API.
+*/
 
 #ifndef SORD_SORDMM_HPP
 #define SORD_SORDMM_HPP

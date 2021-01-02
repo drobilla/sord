b6b115696ce96c1f3d82a51dd4e6d37d1135f452
David Robillard <d@drobilla.net>
2021-01-02 13:28:46 +0100

    Update date in version output

--- src/sordi.c
+++ src/sordi.c
@@ -43,7 +43,7 @@ static int
 print_version(void)
 {
   printf("sordi " SORD_VERSION " <http://drobilla.net/software/sord>\n");
-  printf("Copyright 2011-2016 David Robillard <d@drobilla.net>.\n"
+  printf("Copyright 2011-2021 David Robillard <d@drobilla.net>.\n"
          "License: <http://www.opensource.org/licenses/isc>\n"
          "This is free software; you are free to change and redistribute it."
          "\nThere is NO WARRANTY, to the extent permitted by law.\n");
